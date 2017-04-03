68ef899481827c7306bf7356f79acb13de6e940b
Abderrafie <Abderrafie@uranus>
2017-04-03 16:09:44 +0200

    test

---
+++ testgithub/src/main/java/com/kmse/hicp/cxf/webservice/test/App.java
@@ -0,0 +1,13 @@
+package com.kmse.hicp.cxf.webservice.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
